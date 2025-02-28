81bd86eaef3c6b495cd0d7c575b7bd62ea589c3c
natanDini <natangsdiniz@gmail.com>
2025-02-28 19:54:59 -0300

    Creating connection with DataBase

---
+++ backend/src/main/java/br/com/petsalus/config/DataSourceConfig.java
@@ -0,0 +1,45 @@
+package br.com.petsalus.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import jakarta.persistence.EntityManagerFactory;
+
+@Configuration
+@EnableJpaRepositories(basePackages = "br.com.undf.sgaic.repository")
+public class DataSourceConfig {
+
+    @Value("${spring.datasource.url}")
+    private String url;
+
+    @Value("${spring.datasource.username}")
+    private String username;
+
+    @Value("${spring.datasource.password}")
+    private String password;
+
+    @Value("${spring.datasource.driver-class-name}")
+    private String driverClassName;
+
+    @Bean
+    public DataSource dataSource() {
+    	return DataSourceBuilder.create()
+                .url(url)
+                .username(username)
+                .password(password)
+                .driverClassName(driverClassName)
+                .build();
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager(EntityManagerFactory dataSource) {
+        return new JpaTransactionManager(dataSource);
+    }
+}
